1ee1e4f22a2b5976900407add0f81a70eaa10f58
Severin Kilchhofer <>
2018-11-16 13:04:18 +0100

    added stuff

---
+++ hover.js
@@ -0,0 +1,27 @@
+function mouseenterkombo1(){
+    document.getElementById("infoLinkgroup").style.opacity = "1";
+    document.getElementById("haus").style.opacity = "1";
+    document.getElementById("kreisHaus").style.opacity = "0";
+
+}
+
+function onmouseleavekombo1(){
+    document.getElementById("infoLinkgroup").style.opacity = "0";
+    document.getElementById("haus").style.opacity = "0";
+    document.getElementById("kreisHaus").style.opacity = "1";
+
+}
+
+function mouseenterkombo2(){
+    document.getElementById("infoBus").style.opacity = "1";
+    document.getElementById("bus").style.opacity = "1";
+    document.getElementById("kreisBus").style.opacity = "0";
+
+}
+
+function onmouseleavekombo2(){
+    document.getElementById("infoBus").style.opacity = "0";
+    document.getElementById("bus").style.opacity = "0";
+    document.getElementById("kreisBus").style.opacity = "1";
+
+}
